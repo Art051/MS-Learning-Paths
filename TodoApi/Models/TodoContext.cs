11e12079d907bae785503c5f30983d08e67a3e93
Art051 <91856576+Art051@users.noreply.github.com>
2022-01-27 11:06:20 +0000

    Added TodoApi - MS tutorial for producing Web API and using Swagger / OpenAPI

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApi.Models
+{
+#pragma warning disable CS1591
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+    }
+#pragma warning restore CS1591
+}
