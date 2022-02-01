1e253f41928723c9374aed0234906b46510346ff
Art051 <91856576+Art051@users.noreply.github.com>
2022-02-01 11:51:11 +0000

    Added course: Persist and retrieve relational data with Entity Framework Core.
     Learning objectivesl:
    
        Review object-relational mapper (ORM) concepts.
        Understand Entity Framework Core (EF Core) architecture.
        Write C# code to interact with a database using EF Core.
        Use EF Core Migrations to keep the code and database in sync.
        Use reverse engineering to generate entity models from an existing database.
        Seed a database on app startup.

---
+++ Persist-Retrieve-Entity-Framework/ContosoPizza/Migrations/20220201103729_ModelRevisions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    public partial class ModelRevisions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Calories",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "Pizzas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+        }
+    }
+}
